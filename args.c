7540d49df647756f039e0f96cf44ef706bc72b22
Fernando Montero Erustes <fmontero@c1r14s4.42madrid.com>
2025-07-10 17:27:41 +0200

    empiezo a escribir

---
+++ args.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   args.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmontero <fmontero@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/10 14:01:04 by fmontero          #+#    #+#             */
+/*   Updated: 2025/07/10 17:22:45 by fmontero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int ft_check_args(int argc, char *argv[])
+{
+	if (!(argc == 4 || argc == 5))
+		return (1);
+	while (*argv != NULL)
+		ft_str_to_valid_arg(*argv);
+	return ;
+}
+
+int ft_str_to_valid_arg(char *str)
+{
+	int nmb;
+	int len;
+
+	if (str == NULL)
+		return (-1);
+	while (*str == ' ' || (*str >= '\t' && *str <= '\r'))
+		str++;
+	if (*str == '+')
+		str++;
+	if (*str == '\0')
+		return (-1);
+	while (*str == '0')
+		str++;
+	if (*str == '\0')
+		return (0);
+	len = 0;
+	while (str[len] >= '0' && str[len] <= '9')
+		len++;
+	if (len > 6)
+		return (-1);
+	nmb = 0;
+	while (*str >= '0' && *str <= '9')
+		nmb = nmb * 10 + (*str++ - '0');
+	if (*str != '\0' || nmb >= 1000000)
+		return (-1);
+	return (nmb);
+}
---
+++ philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmontero <fmontero@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/10 15:30:06 by fmontero          #+#    #+#             */
+/*   Updated: 2025/07/10 15:31:48 by fmontero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+// System libraries
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <stdint.h>
+# include <sys/time.h>
+
+#endif
