fa93e6b2e09f6ee62a7e7df9ce9676c83657dc7f
ermonfer <fmerustes@gmail.com>
2025-07-12 13:27:27 +0200

    casita

--- args.c
+++ args.c
@@ -6,7 +6,7 @@
 /*   By: fmontero <fmontero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 14:01:04 by fmontero          #+#    #+#             */
-/*   Updated: 2025/07/10 18:44:22 by fmontero         ###   ########.fr       */
+/*   Updated: 2025/07/12 12:49:28 by fmontero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,25 +26,23 @@ int ft_str_to_valid_arg(char *str)
 	int nmb;
 	int len;
 
-	if (str == NULL)
-		return (-1);
-	while (*str == ' ' || (*str >= '\t' && *str <= '\r') || *str == '+')
+	while (*str == ' ' || (*str >= '\t' && *str <= '\r'))
+		str++;
+	if (*str == '+')
 		str++;
 	if (*str == '\0')
 		return (-1);
 	while (*str == '0')
 		str++;
-	if (*str == '\0')
-		return (0);
-	len = 0;
+	len = 1;
 	nmb = 0;
 	while (str[len] >= '0' && str[len] <= '9')
 	{
-		if (len > 1)
+		if (len > 6)
 			return (-1);
 		nmb = nmb * 10 + (str[len++] - '0');
 	}
-	if (str[len] != '\0' || nmb >= 1000000)
+	if (str[len] != '\0')
 		return (-1);
 	return (nmb);
 }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: fmontero <fmontero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 15:30:06 by fmontero          #+#    #+#             */
-/*   Updated: 2025/07/10 15:31:48 by fmontero         ###   ########.fr       */
+/*   Updated: 2025/07/12 13:16:00 by fmontero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,16 @@
 # include <pthread.h>
 # include <stdint.h>
 # include <sys/time.h>
+
+// Structs
+
+typedef struct s_config {
+	int		n_philos;
+	int		time_to_die;
+	int		time_to_eat;
+	int		time_to_sleep;
+	int		meals_required;
+}	t_config;
+
 
 #endif
